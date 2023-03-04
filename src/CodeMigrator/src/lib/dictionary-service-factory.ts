668abe8d56282fd87fcb22ec4b2e45d9bc2e6a82
Jorge Cardenas <jorge.cardenas@verndale.com>
2023-03-03 22:54:20 -0500

    Added source next js

---
+++ src/CodeMigrator/src/lib/dictionary-service-factory.ts
@@ -0,0 +1,32 @@
+import {
+  DictionaryService,
+  RestDictionaryService,
+  GraphQLDictionaryService,
+  constants,
+} from '@sitecore-jss/sitecore-jss-nextjs';
+import config from 'temp/config';
+
+export class DictionaryServiceFactory {
+  create(): DictionaryService {
+    return process.env.FETCH_WITH === constants.FETCH_WITH.GRAPHQL
+      ? new GraphQLDictionaryService({
+          endpoint: config.graphQLEndpoint,
+          apiKey: config.sitecoreApiKey,
+          siteName: config.jssAppName,
+          rootItemId: '{860BE48F-A2C4-4245-BA0D-DACD436A1B88}',
+          /*
+            The Dictionary Service needs a root item ID in order to fetch dictionary phrases for the current
+            app. If your Sitecore instance only has 1 JSS App, you can specify the root item ID here;
+            otherwise, the service will attempt to figure out the root item for the current JSS App using GraphQL and app name.
+            rootItemId: '{GUID}'
+          */
+        })
+      : new RestDictionaryService({
+          apiHost: config.sitecoreApiHost,
+          apiKey: config.sitecoreApiKey,
+          siteName: config.jssAppName,
+        });
+  }
+}
+
+export const dictionaryServiceFactory = new DictionaryServiceFactory();
